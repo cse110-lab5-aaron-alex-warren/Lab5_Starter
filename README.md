069cebe90661d94bf757358ad31b4f506bbc158a
Aaron-石雪岩 <43869308+Aaron3963@users.noreply.github.com>
2024-05-04 20:41:39 -0700

    Testing with npm test done

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 - Aaron Shi
 - Alex Turco
 - Warren
+
+1) Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.\
+    No I will not. THe `message` feature is vague and hard to define in unit tests. This feature also sounds like a part which users will interact, bringing in unpredictability.
+
+2) Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters.\
+    Yes I will. This is an easy task for unit test. If The test is able to input 81 characters, the test fails.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -8,4 +8,89 @@ import {
   isHexColor,
 } from '../code-to-unit-test/unit-test-me';
 
-// TODO - Part 2
+describe("PhoneNumber Tests", () => {
+  test('Test 1', () => {
+    expect(isPhoneNumber("(110) 101-4075)")).toBe(true);
+  });
+
+  test('Test 2', () => {
+    expect(isPhoneNumber("110-101-4075")).toBe(true);
+  });
+
+  test('Test 3', () => {
+    expect(isPhoneNumber("1101014075")).toBe(false);
+  });
+
+  test('Test 4', () => {
+    expect(isPhoneNumber("7355608")).toBe(false);
+  });
+});
+
+
+describe('Email Tests', () => { 
+  test('Test 1', () => {
+    expect(isEmail("ucsd@ucsd.edu")).toBe(true);
+  });
+
+  test('Test 2', () => {
+    expect(isEmail("im@cse.edu")).toBe(true);
+  });
+
+  test('Test 3', () => {
+    expect(isEmail("ucsd@ucsd.eduTAT")).toBe(false);
+  });
+  test('Test 4', () => {
+    expect(isEmail("ucsd@ucsd.cse.edu")).toBe(false);
+  });
+ })
+
+
+describe('Password Tests', () => { 
+  test('Test 1', () => {
+    expect(isStrongPassword("qwe123")).toBe(true);
+  });
+  test('Test 2', () => {
+    expect(isStrongPassword("password")).toBe(true);
+  });
+  test('Test 3', () => {
+    expect(isStrongPassword("1111111")).toBe(false);
+  });
+  test('Test 4', () => {
+    expect(isStrongPassword("This_is_password_greater_than15")).toBe(false);
+  });
+ })
+
+
+describe('Date Tests', () => { 
+  test('Test 1', () => {
+    expect(isDate("05/04/2024")).toBe(true);
+  });
+
+  test('Test 2', () => {
+    expect(isDate("30/04/2024")).toBe(true);
+  });
+
+  test('Test 3', () => {
+    expect(isDate("05-04-2024")).toBe(false);
+  });
+
+  test('Test 4', () => {
+    expect(isDate("05-04-924")).toBe(false);
+  });
+ })
+
+
+ describe('Hex Tests', () => { 
+  test('Test 1', () => {
+    expect(isHexColor("#FFFFFF")).toBe(true);
+  });
+  test('Test 2', () => {
+    expect(isHexColor("#010101")).toBe(true);
+  });
+  test('Test 1', () => {
+    expect(isHexColor("color")).toBe(false);
+  });
+  test('Test 2', () => {
+    expect(isHexColor("F010101")).toBe(false);
+  });
+  })
